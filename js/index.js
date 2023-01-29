132b76afd34a73adbec9ca80aa6bd3162e5ac482
Ilya Yalymov <chezeee@gmail.com>
2023-01-29 21:57:47 +0300

    Initial structure added

---
+++ js/index.js
@@ -0,0 +1,11 @@
+import User from "./user.js";
+
+// your code
+
+// do not modify
+const user = new User("Sam", "Smith", 30);
+console.log(user); // User {firstName: 'Sam', lastName: 'Smith', age: 30}
+
+const writer = new Writer("Tom", "Roth", 20);
+console.log(writer); // Writer {firstName: 'Tom', lastName: 'Roth', age: 20}
+console.log(writer.publishArticle()); // Article published
